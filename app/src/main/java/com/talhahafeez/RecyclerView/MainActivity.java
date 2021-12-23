2dd940caca40a3fcad9a2423a1ee3cbaee50813d
Shan Zafar <bitf18545@pucit.edu.pk>
2021-12-23 23:26:42 +0500

    A commit

--- app/src/main/java/com/talhahafeez/RecyclerView/MainActivity.java
+++ app/src/main/java/com/talhahafeez/RecyclerView/MainActivity.java
@@ -27,14 +27,14 @@ public class MainActivity extends AppCompatActivity {
         Employees f0 = new Employees(1,"James", R.drawable.image3);
         Employees f1 = new Employees(2,"Leonardo", R.drawable.image1);
         Employees f2 = new Employees(3,"Mario", R.drawable.image4);
-        Employees f3 = new Employees(4,"Martin",R.drawable.image1);
+        Employees f3 = new Employees(4,"Martin",R.drawable.image3);
         Employees f4 = new Employees(5,"Charles", R.drawable.image2);
-        Employees f5 = new Employees(6,"Oliver",R.drawable.image3);
+        Employees f5 = new Employees(6,"Oliver",R.drawable.image1);
         Employees f6 = new Employees(7,"Emma", R.drawable.image3);
-        Employees f7 = new Employees(8,"Kate",R.drawable.image1);
+        Employees f7 = new Employees(8,"Kate",R.drawable.image2);
         Employees f8 = new Employees(8,"Elizebeth",R.drawable.image4);
-        Employees f9 = new Employees(8,"Rupenzel",R.drawable.image2);
-        Employees f10 = new Employees(8,"Iris",R.drawable.image4);
+        Employees f9 = new Employees(8,"Rupenzel",R.drawable.image1);
+        Employees f10 = new Employees(8,"Iris",R.drawable.image3);
         Employees f11 = new Employees(8,"Hamlet",R.drawable.image2);
 
         employeesList.addAll(Arrays.asList(new Employees[]{f0,f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11}));
